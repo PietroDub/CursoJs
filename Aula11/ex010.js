95c8adf5df1ba15114e01c49aa8cb397824298f4
PietroDub <pietroti453@gmail.com>
2025-04-18 22:32:03 -0300

    exericios 10 a 15

---
+++ Aula11/ex010.js
@@ -0,0 +1,16 @@
+var vel = 30.5
+console.log(`A velocidade é ${vel} km/h`)
+
+if (vel > 60){
+    console.log("Você ultrapassou a velocidade, Multado")
+}
+
+console.log ("Dirija sempre com segurança")
+
+var pais = "Brasil"
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Brasileiro!')
+} else {
+    console.log('Estrangeiro!')
+}
---
+++ Aula12/ex014.js
@@ -0,0 +1,12 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora}`)
+if (hora > 5 && hora < 12) {
+    console.log("Bom dia!")
+} else if (hora <= 18) {
+    console.log("Boa tarde!")
+} else {
+    console.log("Boa noite!")
+} if (hora > 23 && hora < 5){
+    console.log("Boa madrugada!")
+}
